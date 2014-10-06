97b64bc74307cd07e0ff8275635a472d7ead83a0
Kjell Otto <otto.kjell@gmail.com>
2014-10-06 22:48:08 +0100

    added tests for extension method ;D better late then never

---
+++ EasyNetQTalk/EasyNetQTalk.Tests/PointTests.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace EasyNetQTalk.Core.Tests
+{
+    [TestFixture]
+    public class PointTests
+    {
+        [Test]
+        [TestCase(0, 0, "II")]
+        [TestCase(250, 0, "I")]
+        [TestCase(0, 250, "III")]
+        [TestCase(250, 250, "IV")]
+        public void PointTest(int x, int y, String expectedQuadrant)
+        {
+            var point = new Point(x, y);
+            Assert.That(point.GetQuadrant(), Is.EqualTo(expectedQuadrant));
+        }
+    }
+}
